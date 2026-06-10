2850200eb96d6795b2352280dd3056c9716a7d71
Douglas DeMaio <ddemaio@suse.de>
2026-06-10 10:17:07 +0200

    changes made for sponsors
    updated logos and random sponsors shown on the footer of the wiki with the bento theme

--- common-yaml2/javascript/sponsors.js
+++ common-yaml2/javascript/sponsors.js
@@ -1,27 +1,27 @@
 /*
-*
 * JavaScript function for opensuse.org
-*
 * Slideshow for sponsor logos
-*
 * Author: Frank Sundermeyer <fs@opensuse.org>
-* $Id: $
 */
-
-function showSponsor () {
+function showSponsor() {
     var IMGLINK = new Array();
-    /**
-    ** Images and Links
-    **/
-    IMGLINK[0] = new Array("http://en.opensuse.org/Sponsors/AMD","http://files.test.opensuse.org/opensuse/en/c/cc/Amd.jpg");
-    IMGLINK[1] = new Array("http://en.opensuse.org/Sponsors/IP_Exchange","http://files.test.opensuse.org/opensuse/en/0/0f/Ip-exchange.jpg");
-    IMGLINK[2] = new Array("http://en.opensuse.org/Sponsors/foo","http://en.opensuse.org/skins/common/images/icons/fileicon-pdf.png");
+    IMGLINK[0] = new Array("https://en.opensuse.org/Sponsors#Platinum_sponsors_of_openSUSE","https://en.opensuse.org/images/9/97/53863A_AMD_E_Blk_RGB.png");
+    IMGLINK[1] = new Array("https://en.opensuse.org/Sponsors#Fastly,_Inc","https://en.opensuse.org/images/1/1c/Fastly-Inc-Logo.png");
+    IMGLINK[2] = new Array("https://en.opensuse.org/Sponsors#SUSE","https://en.opensuse.org/images/c/cb/Suselogo.png");
+    IMGLINK[3] = new Array("https://en.opensuse.org/Sponsors#Silver_Sponsors_of_openSUSE","https://en.opensuse.org/images/2/2b/B1.png");
+    IMGLINK[4] = new Array("https://en.opensuse.org/Sponsors#NorthC","https://en.opensuse.org/images/2/21/NorthC.png");
+    IMGLINK[5] = new Array("https://en.opensuse.org/Sponsors#rsync.net","https://en.opensuse.org/images/2/2f/Rsync.png");
+    IMGLINK[6] = new Array("https://en.opensuse.org/Sponsors#Core_Backbone","https://en.opensuse.org/images/4/4c/Corebackbone.png");
+    IMGLINK[7] = new Array("https://en.opensuse.org/Sponsors#Heinlein_Support","https://en.opensuse.org/images/1/1d/Heinlein-support.png");
+    IMGLINK[8] = new Array("https://en.opensuse.org/Sponsors#MARVELL","https://en.opensuse.org/images/b/b4/Marvell.png");
 
     var INTERVAL = 3; // in seconds
-    var RANDOM = Math.round(Math.random()*(IMGLINK.length-1));
-    setTimeout("showSponsor()",INTERVAL*1000);
+    var RANDOM = Math.floor(Math.random() * IMGLINK.length);
 
-    document.getElementById("sponsorSlidehref").href=IMGLINK[RANDOM][0];
-    document.getElementById("sponsorSlideimg").src=IMGLINK[RANDOM][1];
+    document.getElementById("sponsorSlidehref").href = IMGLINK[RANDOM][0];
+    document.getElementById("sponsorSlideimg").src   = IMGLINK[RANDOM][1];
+
+    setTimeout(showSponsor, INTERVAL * 1000);
+}
 }
 
--- common-yaml3/javascript/common/showSponsors.js
+++ common-yaml3/javascript/common/showSponsors.js
@@ -8,12 +8,12 @@
 * $Id: $
 */
 
-function showSponsors () {
-    var INTERVAL = 3; // in seconds
-    var RANDOM = Math.round(Math.random()*(SPONSORS.length-1));
-
-    setTimeout("showSponsors()",INTERVAL*1000);
-    document.getElementById("sponsorSlidehref").href=SPONSORS[RANDOM][0];
-    document.getElementById("sponsorSlideimg").src=SPONSORS[RANDOM][1];
+var SPONSOR_IDX = 0;
+function showSponsors() {
+    var INTERVAL = 3;
+    document.getElementById("sponsorSlidehref").href = SPONSORS[SPONSOR_IDX][0];
+    document.getElementById("sponsorSlideimg").src   = SPONSORS[SPONSOR_IDX][1];
+    SPONSOR_IDX = (SPONSOR_IDX + 1) % SPONSORS.length;
+    setTimeout(showSponsors, INTERVAL * 1000);
 }
 
